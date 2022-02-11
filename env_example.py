a67120cfb8e1786d0c0afc65b1746be300f59167
marathamz <hamar82@gmail.com>
2022-02-11 14:25:31 +0300

    lesson1

---
+++ env_example.py
@@ -0,0 +1,19 @@
+import os
+
+# pathlib, glob - good to know
+from dotenv import load_dotenv
+
+# take environment variables from .env.
+load_dotenv("./.env")
+
+key = "USER_NAME"
+user_name = os.getenv(key, None)
+user_name1 = os.environ.get(key, None)
+workbook = os.getenv("WORKBOOK", 0)
+wrong_workbook = os.getenv("WRONG_WORKBOOK", 0)
+
+print(user_name)
+print(user_name1)
+print(workbook)
+print(wrong_workbook)
+print()
